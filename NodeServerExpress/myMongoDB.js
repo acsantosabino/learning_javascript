270b31b406467823e6917736ad7b6ef719d0cd35
Arthur Santos <acsantosabino@gmail.com>
2018-09-29 21:04:06 -0300

    Funções de gerenciamento do DB

--- NodeServerExpress/myMongoDB.js
+++ NodeServerExpress/myMongoDB.js
@@ -12,9 +12,9 @@ function listarVendas() {
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("comercio");
-        dbo.collection("vendas").find({}).toArray(function (err, result) {
+        dbo.collection("vendas").find({}).toArray(function (err, res) {
             if (err) throw err;
-            console.log(JSON.stringify(result));
+            console.log(JSON.stringify(res));
             db.close();
         });
     });
@@ -31,14 +31,27 @@ function incluirVenda(venda) {
         });
     });
 }
-function excluirVenda(cod) {
+function excluirVenda(vendaCod) {
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("comercio");
-        var myquery = { codigo: cod };
-        dbo.collection("vendas").deleteOne(myquery, function (err, obj) {
+        var myquery = { codigo: vendaCod };
+        dbo.collection("vendas").deleteOne(myquery, function (err, res) {
             if (err) throw err;
             console.log("1 document deleted");
+            db.close();
+        });
+    });
+}
+function editaVenda(venda){
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("comercio");
+        var myquery = { codigo: venda.codigo };
+        var newvalues = { $set: venda };
+        dbo.collection("vendas").updateMany(myquery, newvalues, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
             db.close();
         });
     });
@@ -48,7 +61,7 @@ function incluirProdutos(vendaCod, newProdutos) {
         if (err) throw err;
         var dbo = db.db("comercio");
         var myquery = { codigo: vendaCod };
-        var newvalues = { produtos: { $concat : newProdutos} };
+        var newvalues = { $addToSet: { produtos : { $each: newProdutos} }};
         dbo.collection("vendas").updateMany(myquery, newvalues, function (err, res) {
             if (err) throw err;
             console.log("1 document updated");
@@ -56,21 +69,61 @@ function incluirProdutos(vendaCod, newProdutos) {
         });
     });
 }
-listarVendas();
-incluirVenda({ "codigo": "C2V002", "vendedor": "Josias", "valor": 11 });
-listarVendas();
-excluirVenda("C2V002");
-listarVendas();
-incluirProdutos("C2V001",[{"nome":"arroz", "preco":11.50, "qnt":2},{"nome":"feijão","preco":3.80,"qnt":3},{"nome":"almondega","preco":1.30,"qnt":10}]);
-incluirProdutos("C2V001",[{"nome":"microondas", "preco":150, "qnt":1}]);
-// console.log(JSON.stringify(listarVendas()));
-// votar(13);
-// console.log(JSON.stringify(listarVendas()));
-// console.log(JSON.stringify(listarVendas()));
-/*
+function excluirProdutos(vendaCod, newProdutos) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("comercio");
+        var myquery = { codigo: vendaCod };
+        var newvalues = { $pullAll: { produtos : newProdutos }};
+        dbo.collection("vendas").updateMany(myquery, newvalues, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+            db.close();
+        });
+    });
+}
+function calcValorVenda(vendaCod) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("comercio");
+        var myquery = { $match : { codigo: vendaCod }};
+        // var newvalues = { $pullAll: { produtos : newProdutos }};
+        dbo.collection("vendas").aggregate(
+            [{ $match : { codigo: vendaCod }},
+            {$unwind:"$produtos"},
+            { $group : {
+                _id : "$_id",
+                codigo : { $last : "$codigo"},
+                valor : { $sum : { $multiply : ['$produtos.qnt','$produtos.preco']}}
+            }}]
+            ).toArray(function(err, res) {
+                if (err) throw err;
+                console.log(JSON.stringify(res));
+                editaVenda(res[0]);
+                db.close();
+        });
+    });
+}
+// listarVendas();
+// incluirVenda({ "codigo": "C2V002", "vendedor": "Josias", "valor": 11 });
+// listarVendas();
+// excluirVenda("C2V002");
+// listarVendas();
+// incluirProdutos("C2V001",[{"nome":"arroz","preco":11.5,"qnt":2},{"nome":"feijão","preco":3.8,"qnt":3},{"nome":"almondega","preco":1.3,"qnt":10}]);
+// listarVendas();
+// excluirProdutos("C2V001",[{"nome":"arroz","preco":11.5,"qnt":2},{"nome":"feijão","preco":3.8,"qnt":3},{"nome":"almondega","preco":1.3,"qnt":10}]);
+// calcValorVenda("C2V001");
+// editaVenda({"codigo":"C2V001","valor":10,"produtos":[{"nome":"arroz","preco":11.5,"qnt":2},{"nome":"feijão","preco":3.8,"qnt":3},{"nome":"almondega","preco":1.3,"qnt":10}]})
+// listarVendas();
+// editaVenda({"codigo":"C2V001","valor":11,"produtos":[{"nome":"microondas","preco":150,"qnt":1}]})
+// listarVendas();
+
 module.exports = {
     listarVendas,
     incluirVenda,
     excluirVenda,
-    votar
-}*/
+    editaVenda,
+    incluirProdutos,
+    excluirProdutos,
+    calcValorVenda
+}
